8fe20437334571c745d417c9d1edd9cf91d3999c
JulWas <julia.wasilewska.18@gmail.com>
2025-03-28 14:33:14 +0100

    commit mobile tests

---
+++ src/test/java/com/zebrunner/carina/gui/BaseTest.java
@@ -0,0 +1,14 @@
+package com.zebrunner.carina.gui;
+
+import com.zebrunner.carina.core.AbstractTest;
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.gui.pages.HomePage;
+import org.testng.annotations.Test;
+
+public class BaseTest extends AbstractTest {
+    @Test
+    public void baseTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+    }
+}
